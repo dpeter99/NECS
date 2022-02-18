da2460ece1a0cbfec3e43ed8709b39776883faa7
dpeter99 <dpeter99@gmail.com>
2022-02-18 19:23:57 +0100

    [SYNC]

--- src/Runtime/Types/ComponentType.cs
+++ src/Runtime/Types/ComponentType.cs
@@ -1,38 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace src.Runtime.Types
 {
+    public class EcsComponentType {
+        // ReSharper disable StaticMemberInGenericType
+        public static readonly int TypeIndex;
+        public static readonly Type Type;
+        // ReSharper restore StaticMemberInGenericType
+    }
+    
     /// <summary>
     /// Global descriptor of used component type.
     /// </summary>
     /// <typeparam name="T">Component type.</typeparam>
-    public static class EcsComponentType<T> where T : struct {
+    public class EcsComponentType<T> : EcsComponentType where T : unmanaged {
         // ReSharper disable StaticMemberInGenericType
         public static readonly int TypeIndex;
         public static readonly Type Type;
-        public static readonly bool IsIgnoreInFilter;
-        public static readonly bool IsAutoReset;
         // ReSharper restore StaticMemberInGenericType
 
         static EcsComponentType () {
-            TypeIndex = Interlocked.Increment (ref EcsComponentPool.ComponentTypesCount);
+            TypeIndex = Interlocked.Increment (ref EcsComponentTypePool.ComponentTypesCount);
             Type = typeof (T);
-            IsIgnoreInFilter = typeof (IEcsIgnoreInFilter).IsAssignableFrom (Type);
-            IsAutoReset = typeof (IEcsAutoReset<T>).IsAssignableFrom (Type);
-            
-            if (!IsAutoReset && Type.GetInterface ("IEcsAutoReset`1") != null) {
-                throw new Exception ($"IEcsAutoReset should have <{typeof (T).Name}> constraint for component \"{typeof (T).Name}\".");
-            }
+
         }
     }
 
-    public sealed class EcsComponentPool {
+    public sealed class EcsComponentTypePool {
         /// <summary>
         /// Global component type counter.
         /// First component will be "1" for correct filters updating (add component on positive and remove on negative).
         /// </summary>
         internal static int ComponentTypesCount;
+
+        internal List<EcsComponentType> _types = new();
+
     }
 }
 
